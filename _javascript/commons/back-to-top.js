a23bd4c1ee7b77eb1603390037ae7c717a279ce0
Ilma Aliya Fiddien <ilmaaliyaf@gmail.com>
2023-06-03 15:37:14 +0100

    update scroll up button

--- _javascript/commons/back-to-top.js
+++ _javascript/commons/back-to-top.js
@@ -1,20 +1,18 @@
 /*
 Reference: https://bootsnipp.com/snippets/featured/link-to-top-page
 */
-$(function() {
-  $(window).scroll(() => {
-    if ($(this).scrollTop() > 50 &&
-      $("#sidebar-trigger").css("display") === "none") {
-      $("#back-to-top").fadeIn();
+export function back2top() {
+  const $window = $(window);
+  const $btn = $('#back-to-top');
+
+  $window.on('scroll', () => {
+    if ($window.scrollTop() > 50) {
+      $btn.fadeIn();
     } else {
-      $("#back-to-top").fadeOut();
+      $btn.fadeOut();
     }
   });
 
-  $("#back-to-top").click(() => {
-    $("body,html").animate({
-      scrollTop: 0
-    }, 800);
-    return false;
+  $btn.on('click', () => {
+    $window.scrollTop(0);
   });
-});
